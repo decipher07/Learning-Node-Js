e78e690269c1993da7be24121ed3e2283ecfff69
decipher07 <social.deej@gmail.com>
2020-03-12 03:16:37 +0530

    Learning About Express framework for Backend Development

---
+++ Express_Crash_Course/Members.js
@@ -0,0 +1,14 @@
+const members = [
+    {
+        id : 1 ,
+        name : "John Doe",
+        email : "john@gmail.com"
+    },
+    {
+        id : 2 ,
+        name : "Bob William",
+        email : "bobwillaim@gmail.com"
+    }
+];
+
+module.exports = members;
