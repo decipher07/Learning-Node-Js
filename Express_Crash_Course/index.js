633bd20e4941686cb1b9a12d6bdfd4608fe67342
decipher07 <social.deej@gmail.com>
2020-03-12 03:36:46 +0530

    Setting up a Login Page

--- Express_Crash_Course/index.js
+++ Express_Crash_Course/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const logger = require('./middleware/logger');
-
+const members = require('./Members');
 
 const app = express ();
 
@@ -18,7 +18,10 @@ app.use(express.json());
 app.use(express.urlencoded({extended : false}))
 
 // Homepage Route
-app.get('/', (req,res) => res.render('index'));
+app.get('/', (req,res) => res.render('index', {
+    title: 'Member App',
+    members: members
+}));
 
 // Set Static Folder 
 
